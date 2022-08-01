863e37d3abe3a04a75dd643e7b3714f2d8ae06e0
maksym0797 <maaaaksiiiim@gmail.com>
2022-08-01 12:58:51 +0300

    update import parse

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -1,17 +1,16 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
 import { formatStringResponse } from "@libs/api-gateway";
 import { middyfy } from "@libs/lambda";
-import { getSignedUrlByName } from "@libs/s3";
+import { uploadToS3 } from "@libs/s3";
 
 import schema from "./schema";
 
 const importFileParser: ValidatedEventAPIGatewayProxyEvent<
   typeof schema
 > = async (event) => {
-  const { name } = event.pathParameters;
   try {
-    const url = await getSignedUrlByName(name);
-    return formatStringResponse(url);
+    await uploadToS3();
+    return formatStringResponse("");
   } catch (e) {
     console.log(e);
     return {
--- import-service/src/libs/s3.ts
+++ import-service/src/libs/s3.ts
@@ -3,6 +3,7 @@ import { S3 } from "aws-sdk";
 const BUCKET = "import-bucket-task-5";
 const AWS = require("aws-sdk");
 const s3: S3 = new AWS.S3({ region: "eu-central-1" });
+const csv = require("csv-parser");
 
 export const getSignedUrlByName = async (name: string): Promise<string> => {
   const params = {
@@ -30,10 +31,27 @@ export const uploadToS3 = async () => {
   };
   const catalogs = await s3.listObjectsV2(params).promise();
   for (const catalog of catalogs.Contents) {
-    const params = {
-      Bucket: BUCKET,
-      Key: catalog,
-    };
-    const s3Stream = s3.getObject(params).createReadStream();
+    const results = await readFileAsync(catalog.Key);
+    console.log(results);
   }
+};
+
+const readFileAsync = (
+  catalogKey: string
+): Promise<Record<string, unknown>> => {
+  const params = {
+    Bucket: BUCKET,
+    Key: catalogKey,
+  };
+  const results: Record<string, unknown>[] = [];
+  return new Promise((resolve, reject) => {
+    s3.getObject(params)
+      .createReadStream()
+      .pipe(csv())
+      .on("data", (data) => results.push(data))
+      .on("end", () => {
+        resolve(results);
+      })
+      .on("error", (error) => reject(error));
+  });
 };
